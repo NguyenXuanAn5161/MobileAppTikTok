1beb0e3e022cbbe865066749f79df5111defab7e
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-11-04 08:29:37 +0700

    format code

--- tiktok/src/component/ProfileScreenComponent.js
+++ tiktok/src/component/ProfileScreenComponent.js
@@ -1,11 +1,10 @@
-import { AntDesign, Feather } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Avatar } from 'react-native-paper';
+import { AntDesign, Feather } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Avatar } from "react-native-paper";
 
 const BottomTab = createBottomTabNavigator();
-
 
 export default function Profile() {
   return (
@@ -16,14 +15,19 @@ export default function Profile() {
         </TouchableOpacity>
         <TouchableOpacity>
           <Text style={styles.text}>Xuan An</Text>
-          </TouchableOpacity>
-          <AntDesign name="caretdown" size={10} color="black" style={styles.text1}/>
+        </TouchableOpacity>
+        <AntDesign
+          name="caretdown"
+          size={10}
+          color="black"
+          style={styles.text1}
+        />
         <TouchableOpacity>
           <Feather name="menu" size={22} style={styles.text2} />
         </TouchableOpacity>
       </View>
       <View style={styles.userName}>
-        <Avatar.Icon size={80} icon={"account"}/>
+        <Avatar.Icon size={80} icon={"account"} />
         <Text style={styles.accountText}>@xuanan</Text>
       </View>
       <View style={styles.counterCount}>
@@ -42,99 +46,101 @@ export default function Profile() {
       </View>
       <View style={styles.userButton}>
         <View>
-        <TouchableOpacity style={styles.grayOutLineButton}>
-          <Text style={{fontWeight: 'bold', fontSize: 15}}>Edit profile</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.grayOutLineButton}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+              Edit profile
+            </Text>
+          </TouchableOpacity>
         </View>
-        <View style={{paddingLeft:30}}>
-        <TouchableOpacity style={styles.grayOutLineButton1}>
-        <Feather name="bookmark" size={18} color="black" style={{textAlign: 'center'}} />
-        </TouchableOpacity>
+        <View style={{ paddingLeft: 30 }}>
+          <TouchableOpacity style={styles.grayOutLineButton1}>
+            <Feather
+              name="bookmark"
+              size={18}
+              color="black"
+              style={{ textAlign: "center" }}
+            />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-      flex: 1,
-    },
-    navbar:{
-        flexDirection: 'row',
-        paddingHorizontal: 20,
-        paddingVertical: 15,
-        backgroundColor: 'white',
-        borderBottomWidth: 1,
-        borderColor: 'black',
-        flex: 1,
-        justifyContent:'space-between'
-       
-    },
-    text:{
-      fontSize: 19,
-      color: 'black',
-      textAlign: 'center',
-      fontWeight: 'bold',
-    },
-    text1:
-    {
-      marginLeft:'-90px',
-      marginTop:'10px'
-    },
-    text2:{
-      marginTop: '5px'
-    },
-    userName:{
-      paddingVertical: 20,
-      alignItems: 'center',
-      backgroundColor: 'white',
-      borderColor: 'black',
-    },
-    accountText:{
-      padding: 20,
-      fontWeight: 'bold',
-      fontSize: 17,
-    },
-    counterCount:{
-      backgroundColor: 'white',
-      flexDirection: 'row',
-      paddingHorizontal: 50,
-      paddingBottom: 10,
-      marginTop:'-30px'
-    },
-    counterItems:{
-      flex: 1,
-      alignItems: 'center',
-    },
-    countText:{
-      fontWeight: 'bold',
-      fontSize: 16,
-    },
-    countLabel:{
-      color: 'gray',
-      fontSize: 11,
-    },
-    userButton:{
-      backgroundColor: 'white',
-      flexDirection: 'row',
-      justifyContent:'center'
-    },
-    grayOutLineButton:{
-      borderColor: 'lightgray',
-      borderWidth: 1,
-      borderRadius: 4,
-      paddingVertical: 10,
-      paddingHorizontal: 30,
-      
-    },
-    grayOutLineButton1:{
-      borderColor: 'lightgray',
-      borderWidth: 1,
-      borderRadius: 4,
-      width: '45px',
-      paddingVertical: 10,
-    }
-      
-
-})
+  container: {
+    flex: 1,
+  },
+  navbar: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderColor: "black",
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 19,
+    color: "black",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  text1: {
+    marginLeft: "-90px",
+    marginTop: "10px",
+  },
+  text2: {
+    marginTop: "5px",
+  },
+  userName: {
+    paddingVertical: 20,
+    alignItems: "center",
+    backgroundColor: "white",
+    borderColor: "black",
+  },
+  accountText: {
+    padding: 20,
+    fontWeight: "bold",
+    fontSize: 17,
+  },
+  counterCount: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    paddingHorizontal: 50,
+    paddingBottom: 10,
+    marginTop: "-30px",
+  },
+  counterItems: {
+    flex: 1,
+    alignItems: "center",
+  },
+  countText: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  countLabel: {
+    color: "gray",
+    fontSize: 11,
+  },
+  userButton: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  grayOutLineButton: {
+    borderColor: "lightgray",
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+  },
+  grayOutLineButton1: {
+    borderColor: "lightgray",
+    borderWidth: 1,
+    borderRadius: 4,
+    width: "45px",
+    paddingVertical: 10,
+  },
+});
